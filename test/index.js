585f697e4281e6927a144ec343d81aa87e8b0a5f
Zakk Fleischmann <fleischmann@teamsubjectmatter.com>
2018-02-26 10:43:16 -0500

    update tests and instructions

--- test/index.js
+++ test/index.js
@@ -26,74 +26,81 @@ describe("Setup -", () => {
   });
 
   it("should have an index.js", done => {
-    let reqIndex = () => require('../index.js')
+    let reqIndex = () => require("../index.js");
 
-    expect(reqIndex).to.not.throw()
+    expect(reqIndex).to.not.throw();
     expect(reqIndex).to.not.be.undefined;
     done();
   });
 
   it("should have a connection.js", done => {
-    let reqConnection = () => require('../db/connection.js')
+    let reqConnection = () => require("../db/connection.js");
 
-    expect(reqConnection).to.not.throw()
+    expect(reqConnection).to.not.throw();
     expect(reqConnection).to.not.be.undefined;
     done();
   });
 
   it("should have a handlebars layout", done => {
-    let reqLayout = () => require('../views/layout.hbs')
+    let reqLayout = () => require("../views/layout.hbs");
 
-    expect(reqLayout).to.not.throw()
+    expect(reqLayout).to.not.throw();
     expect(reqLayout).not.to.be.undefined;
     done();
   });
 
   it("should have an index handlebars template", done => {
-    let reqIndexView = () => require('../views/index.hbs')
+    let reqIndexView = () => require("../views/index.hbs");
 
-    expect(reqIndexView).to.not.throw()
+    expect(reqIndexView).to.not.throw();
     expect(reqIndexView).not.to.be.undefined;
     done();
   });
 
   it("should have an new messages handlebars template", done => {
-    let reqNewMessageView = () => require("../views/messages/new.hbs")
+    let reqNewMessageView = () => require("../views/messages/new.hbs");
 
-    expect(reqNewMessageView).to.not.throw()
+    expect(reqNewMessageView).to.not.throw();
     expect(reqNewMessageView).not.to.be.undefined;
     done();
   });
 
   it("should have an messages show handlebars template", done => {
-    let reqShowView = () => require("../views/messages/show.hbs")
+    let reqShowView = () => require("../views/messages/show.hbs");
 
-    expect(reqShowView).to.not.throw()
+    expect(reqShowView).to.not.throw();
     expect(reqShowView).not.to.be.undefined;
     done();
   });
 
   it("should have a messages controller", done => {
-    let reqMessagesController = () => require("../controllers/messages.js")
-    
-    expect(reqMessagesController).to.not.throw()
+    let reqMessagesController = () => require("../controllers/messages.js");
+
+    expect(reqMessagesController).to.not.throw();
     expect(reqMessagesController).not.to.be.undefined;
+    done();
+  });
+
+  it("should have a Messages model", done => {
+    let reqModel = () => require("../models/Message");
+    expect(reqModel).to.not.throw;
+    expect(reqModel).not.to.be.undefined;
     done();
   });
 });
 
 describe("Model -", () => {
-  let mongoose
-  let Message
-  let app
+  let mongoose;
+  let Message;
+  let app;
 
   before(done => {
     mongoose = require("../db/connection");
-    Message = mongoose.model("Message");
+    Message = require("../models/Message");
     app = require("../index.js");
-    done()
-  })
-  
+    done();
+  });
+
   beforeEach(done => {
     let Message = mongoose.model("Message");
     Message.remove({}).then(() => {
@@ -158,17 +165,17 @@ describe("Model -", () => {
 describe("Routes -", () => {
   this.timeout = 10000;
 
-  let mongoose
-  let Message
-  let app
+  let mongoose;
+  let Message;
+  let app;
 
   before(done => {
     mongoose = require("../db/connection");
     Message = mongoose.model("Message");
-    app = require("../index.js");   
-    done() 
-  })
- 
+    app = require("../index.js");
+    done();
+  });
+
   beforeEach(done => {
     Message.remove({}).then(() => {
       Message.create({
@@ -251,10 +258,8 @@ describe("Routes -", () => {
         .request(app)
         .post("/messages")
         .send({
-          message: {
-            author: "Christopher Robin",
-            body: "Where is Pooh Bear?"
-          }
+          author: "Christopher Robin",
+          body: "Where is Pooh Bear?"
         })
         .end((err, res) => {
           Message.findOne({ author: "Christopher Robin" }).then(message => {
