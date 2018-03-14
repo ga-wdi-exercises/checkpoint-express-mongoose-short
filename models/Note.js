77859492eb435fcc31655f7f275ca8fb0c0d3b81
Laura Haft <Lchaft@gmail.com>
2018-03-14 09:14:35 -0400

    some things working

--- models/Note.js
+++ models/Note.js
@@ -1,5 +1,9 @@
 const mongoose = require("../db/connection");
 
-const Note = new mongoose.Schema({});
+const Note = new mongoose.Schema({
+   title: String,
+   author: String,
+   body: String
+});
 
 module.exports = mongoose.model("Note", Note);
