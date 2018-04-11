385bff9ca5b36f6a433765f6b23f0e552d5ae184
Jason Toups <jason.p.toups@gmail.com>
2018-04-11 09:23:02 -0400

    set up file structure and add boilerplate to index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const express = require('express')
+const app = express()
+const hbs = require('hbs')
+const notesController = require('./controllers/notes')
+
+app.set('view engine', 'hbs')
+app.use('/', notesController)
+
 app.listen(3000, () => console.log('app is running'))
 
 // DO NOT REMOVE THIS LINE:
