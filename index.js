5cf9f5bfa7a5af03e531f1d0f0bc86544ae14229
Minhquan Thai <minhquanthai@gmail.com>
2018-04-11 09:26:20 -0400

    Required hbs and express in index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const express = require('express')
+const hbs = require('hbs')
+const app = express()
+
+app.set('view engine', 'hbs')
+
 app.listen(3000, () => console.log('app is running'))
 
 // DO NOT REMOVE THIS LINE:
