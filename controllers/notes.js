60e2c95654f146ffbc63a19907f7ddd8bfc96828
Minhquan Thai <minhquanthai@gmail.com>
2018-04-11 09:51:13 -0400

    Worked on seeds

--- controllers/notes.js
+++ controllers/notes.js
@@ -3,7 +3,13 @@ const router = express.Router()
 const Notes = require('../models/Note')
 
 router.get('/', (req, res) => {
-  Notes.find({}).then(notes => res.render('index', {notes}))
+  Notes.find({})
+    .then(notes => res.render('index', {notes}))
+})
+
+router.get('/notes/:id', (req, res) => {
+  Notes.findOne({_id: req.params.id})
+    .then(note => res.render('show', {note}))
 })
 
 module.exports = router
