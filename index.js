2af0109d9285fd0a679dd9d67cc97a2a2edfbdc1
Daniel Lousa <dlousa92@gmail.com>
2018-06-18 09:47:49 -0400

    fix which route i want to use

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 app.set('view engine', 'hbs')
 
 app.use('/', require('./routes/app'))
-app.use('/note', require('./routes/note'))
+app.use('/', require('./routes/note'))
 app.listen(3000, () => console.log('app is running'))
 
 // DO NOT REMOVE THIS LINE:
