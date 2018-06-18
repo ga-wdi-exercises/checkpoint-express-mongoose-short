a4a1e84f7126e2bdbec51e83225258a4447c96bc
Malik Keenen Ahmed <kansomalik@gmail.com>
2018-06-18 10:00:08 -0400

    check express

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+
+app.get('/',(req, res) => {
+  res.render('/notes')
+})
+app.get('/notes', (req, res) => {
+    Notes.find({})
+    .then(Notes => {
+        res.render('index', { Notes })
+    })
+})
+app.get('/:author', (req, res) => {
+    Notes.findOne({author: req.params.author})
+    .then(Notes => {
+        res.render('note/show', note)
+    })
+})
 app.listen(3000, () => console.log('app is running'))
 
 // DO NOT REMOVE THIS LINE:
