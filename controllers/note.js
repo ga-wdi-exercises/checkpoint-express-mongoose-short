e2e16ae4af5002ef6fa491310656a869b15a9d6c
Daniel Lousa <dlousa92@gmail.com>
2018-06-18 10:00:12 -0400

    populate index to show notes

--- controllers/note.js
+++ controllers/note.js
@@ -2,7 +2,14 @@ const Note = require('../models/Note')
 
 module.exports = {
   notes: (req, res) => {
-    res.send('list of notes is working')
+    Note.find({})
+      .populate({
+        path: 'notes',
+        options: {sort: { createdAt: -1 }}
+      })
+      .then(note => { 
+        res.render('index', { note })
+      })
   },
   show: (req, res) => {
     console.log('Working')
