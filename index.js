51bba7ace619793c502a8e2fb6c4ffa4956048fa
Joshua Nicholas Pearson <jnpearson4850@gmail.com>
2018-07-30 09:15:00 -0400

    have 7 of 3 done, installed hbs and mongoose, issues with express

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-app.listen(3000, () => console.log('app is running'))
+const express = require("express")
+const app = express()
+
+
+app.listen(3000, () => {
+    console.log('app is running on 3000'))}
 
 // DO NOT REMOVE THIS LINE:
 module.exports = app
