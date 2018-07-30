4e78916949b6da4c560cecc1633098cd7269bd92
DavidZ <david.zafirovic@gmail.com>
2018-07-30 10:05:08 -0400

    Checkpoint done

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+const express = require("express");
+const hbs = require("hbs");
+
+
+
+const app = express()
+
+app.get('/notes', function(req, res) {
+	res.render('/')
+})
+app.get('/', function(req, res) {
+	res.send('/')
+})
+
+app.get('/notes/:author', function(req, res) {
+	res.send('/')
+})
 app.listen(3000, () => console.log('app is running'))
+
+
 
 // DO NOT REMOVE THIS LINE:
 module.exports = app
