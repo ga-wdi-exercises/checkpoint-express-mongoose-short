3853e9f00e9056b30ba379a80b21da2a28b5a680
Boku <Boku@NHs-MacBook-Pro.local>
2018-07-30 09:55:53 -0400

    Modified const Note entry.

---
+++ controllers/note.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+const Note = require("../models/Note");
+
+router.get("/", (req, res) => {
+  Note.find({})
+    .then(notes => {
+      res.render("index", { notes });
+    })
+    .catch(err => console.log(err));
+});
