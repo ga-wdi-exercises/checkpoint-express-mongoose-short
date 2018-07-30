86f7dd1d53c2fe3aacb54364daa16e9aea44ae95
Angelica Harrison <harrison_angelica@yahoo.com>
2018-07-30 10:00:36 -0400

    final upload

--- models/Note.js
+++ models/Note.js
@@ -1,5 +1,16 @@
 const mongoose = require('../db/connection')
+const Schema = mongoose.Schema;
 
-const Note = new mongoose.Schema({})
+const Note = new mongoose.Schema({
+  content: String,
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+})
 
 module.exports = mongoose.model('Note', Note)
