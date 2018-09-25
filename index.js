bef2d9372f585cad2df468570b7b11e09e69453d
Liz Makawa <E.K.Makawa@gmail.com>
2018-09-25 09:59:23 -0400

    Set up controllers, views, routes, installed mongo, body-parser, express..

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const express = require('express')
+const hbs = require("hbs");
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const app = express()
+
+app.use(require('./routes/index.js'))
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'hbs')
+app.use(methodOverride('_method'))
+hbs.registerPartials(__dirname + "/views/partials");
+
 app.listen(3000, () => console.log('app is running'))
 
 // DO NOT REMOVE THIS LINE:
