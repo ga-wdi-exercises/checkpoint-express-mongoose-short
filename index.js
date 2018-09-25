b4a28f8fa4ac02e6c5de9660e082f9371f7da04b
Nick Tartaro <ntartaro@gmail.com>
2018-09-25 09:42:11 -0400

    finished test

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const express = require('express');
+const app = express();
+const router = require('./controllers/notes.js');
+
+app.use(router);
+app.set('view engine', 'hbs');
+
+
 app.listen(3000, () => console.log('app is running'))
 
 // DO NOT REMOVE THIS LINE:
