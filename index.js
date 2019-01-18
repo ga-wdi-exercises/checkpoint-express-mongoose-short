15f3f5da2f379dc39767e17bbd6b732cb52213be
Mifta Hamid <miftahamid@Miftas-MacBook-Air.local>
2019-01-18 11:04:58 -0500

    I need more practice on router and models

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
+const express = require('express');
+
+
+// app.get('/', (res,req) => {
+//     res.send('hello form')
+// })
+// app.get('/notes', (res,req) => {
+//     res.send('hello form/notes')
+// })
+// app.get('/notes/:author', (res,req) => {
+//     res.send('hello form /notes/:author')
+// })
+app.get('/', function (req, res) {
+    res.send('GET request to the homepage')
+  })
+  app.get('/notes', function (req, res) {
+    res.send('GET request from notes')
+  })
+  app.get('/notes/:author', function (req, res) {
+    res.send('GET request from notes author')
+  })
+// app.get('/notes', require('./models/Note'));
+// app.get('/notes/:author', require('./models/Note'));
+
 app.listen(3000, () => console.log('app is running'))
+// router.all('*', (req, res) => {
+//     res.status(3000).send();
+//   });
 
 // DO NOT REMOVE THIS LINE:
 module.exports = app
--- models/Note.js
+++ models/Note.js
@@ -1,5 +1,10 @@
 const mongoose = require('../db/connection')
+const Schema = mongoose.Schema;
 
-const Note = new mongoose.Schema({})
+const Note = new Schema({
+    author: String,
+    title: String,
+    body: String
+})
 
 module.exports = mongoose.model('Note', Note)
