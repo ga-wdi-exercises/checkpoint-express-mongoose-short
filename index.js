d93e6429b192e13007a73b2968525b6937caa7ab
Pierson Brannan <plbrannan95@gmail.com>
2019-01-23 09:52:56 -0500

    npm problems, 80% of test time is going to debugging error messages within npm and test folder

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+
+
+const express = require("express");
+const router = express.Router();
+const hbs = require("hbs");
+const app = express()
+
+
+router.get('/', (req, res) => {
+    res.redirect('/notes')
+})
+
+router.get('/notes', (req, res) => {
+    Note.find({})
+      .populate("Note")
+      .exec(function(err, Note) {
+        res.render("index", Note);
+    // res.render('notes', { Note }
+})})
+
+router.get('/notes/:id', (req, res) => {
+    Note.findOne({_id: req.params.id})
+      .populate("author Note.author")
+      .exec(function(err, Note) {
+       res.render("show", Note);
+})})
+
 app.listen(3000, () => console.log('app is running'))
 
 // DO NOT REMOVE THIS LINE:
--- models/Note.js
+++ models/Note.js
@@ -1,5 +1,11 @@
 const mongoose = require('../db/connection')
+const express = require("express");
+const router = express.Router();
 
-const Note = new mongoose.Schema({})
+const Note = new mongoose.Schema({
+    author: "",
+    title: "",
+    body: ""
+})
 
 module.exports = mongoose.model('Note', Note)
