ae192e377e6547a7354a0de733213bf20f141658
Evan Canoy <evan@Evans-MacBook-Pro.local>
2019-01-23 09:59:39 -0500

    test done

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const express = require('express')
+const app = express()
+const note = require('./models/note')
+
+
+app.get('/', (req, res) => {
+    res.json(note)
+})
+
 app.listen(3000, () => console.log('app is running'))
 
 // DO NOT REMOVE THIS LINE:
--- models/Note.js
+++ models/Note.js
@@ -1,5 +1,11 @@
 const mongoose = require('../db/connection')
 
-const Note = new mongoose.Schema({})
+const Note = new mongoose.Schema({
+    title: String,
+    author: String,
+    body: String
+})
+
+
 
 module.exports = mongoose.model('Note', Note)
