c9cc82117ebdb05c3f7cac94d96bfb880efc9e15
Jake Brackney <brackney.jake@gmail.com>
2019-01-23 09:33:10 -0500

    Model, Route, and Controller File Setup

---
+++ routes/notes.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+const noteController = require('../controllers/note')
+
+router.get('/', require('./notes'));
+router.get('/notes', noteController.show)
+
+module.exports = router;
