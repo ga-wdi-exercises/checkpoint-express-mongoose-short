6702622ad7779088142b0353788f0e2dcfe62a48
Annabelle Taylor <annabelle.t.taylor@gmail.com>
2019-02-26 09:34:27 -0500

    All tests passing

--- controllers/note.js
+++ controllers/note.js
@@ -1,9 +1,15 @@
+const Note = require("../models/Note")
+
 module.exports = {
     index: function(req,res){
-        res.render("index")
+        Note.find({}).then(notes => {
+            res.render("index", { notes })
+        })
     },
 
     show: function(req,res){
-        res.render("show")
+        Note.findOne({ _id: req.params.id }).then(note => {
+            res.render("show", { note })
+        })
     }
 }
