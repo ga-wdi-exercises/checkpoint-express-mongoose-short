425f4d42b7b88f393b5ce99d0c4731789cf28585
Virginia <virginialash1@gmail.com>
2019-02-26 09:14:32 -0500

    inital installs

--- models/Note.js
+++ models/Note.js
@@ -3,3 +3,5 @@ const mongoose = require('../db/connection')
 const Note = new mongoose.Schema({})
 
 module.exports = mongoose.model('Note', Note)
+
+//3. There will be a single model, `Note`, with three fields: an `author`, a `title` and a `body`. Each field expects a string value.
