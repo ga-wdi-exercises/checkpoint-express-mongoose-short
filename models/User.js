17d8411fafacad03f6c48333353754879415750c
James Byess <james.byess@gmail.com>
2019-04-29 22:40:08 -0400

    updates

--- models/User.js
+++ models/User.js
@@ -1,12 +1,5 @@
 const mongoose = require('../db/connection')
 
-const userSchema = new mongoose.Schema({
-	username: String,
-	email: String,
-	notes: [{
-		ref: "Note",
-		type: mongoose.Schema.Types.ObjectId
-	}]
-})
+const userSchema = new mongoose.Schema({})
 
 module.exports = mongoose.model('User', userSchema)
