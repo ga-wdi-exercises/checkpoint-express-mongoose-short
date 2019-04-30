34f377a2dd55a833db06d62f183768a984481b8e
Atasha Potter <Ms.APotter@gmail.com>
2019-04-30 10:00:49 -0400

    tried to adjust routes to get green checkmarks

--- controllers/notes.js
+++ controllers/notes.js
@@ -4,12 +4,14 @@ const router = express.Router()
 const Note = require("../models/Note")
 const User = require("../models/User")
 
-
+//  Need to put this line to get npm test to give green check for "redirect response" 
 router.get('/', (req, res) => {
 	res.redirect('/notes/')
 })
+//
 
-router.get("/notes", (req, res) => {
+
+router.get("/notes/", (req, res) => {
     Notes.find({}).then(allnotes => res.json(allnotes))
 })
 
