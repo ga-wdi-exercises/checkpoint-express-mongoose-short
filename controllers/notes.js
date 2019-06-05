5203ec679945154b0ce893cc243ce17c6a591407
Ellison Rio <rio.ellison@gmail.com>
2019-06-05 09:26:06 -0400

    added some routes to notes and users

---
+++ controllers/notes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const Note = require("../models/Note");
+
+router.get("/notes", (req,res) => {
+    Note.find({})
+    .then(notes => res.json(notes))
+})
+
+router.get("/notes/:_id", (req,res) => {
+    Note.findOne({ id: req.params._id})
+    .then(notes => res.json(notes))
+})
+
+module.exports = router;
---
+++ controllers/users.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+
+router.get("/users", (req,res) => {
+    User.find({})
+    .then(users => res.json(users))
+})
+
+router.get("/users/:_id", (req,res) => {
+    Note.findOne({ id: req.params._id})
+    .then(users => res.json(users))
+})
+
+module.exports = router;
