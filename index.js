48bba7f5a64771441e77fa18939d665b5b5e3f2a
DCartist <pbannerman13@gmail.com>
2019-06-05 09:19:27 -0400

    working on models

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const express = require('express')
+const parser = require('body-parser')
+const app = express()
+
+
+const noteController = require('./controllers/notes.js')
+const userController = require('./controllers/users.js')
+
+
+
 app.listen(3000, () => console.log('app is running'))
 
 // DO NOT REMOVE THIS LINE:
--- models/User.js
+++ models/User.js
@@ -1,5 +1,24 @@
 const mongoose = require('../db/connection')
 
-const userSchema = new mongoose.Schema({})
+const userSchema = new mongoose.Schema({
+    user: {
+        type: String,
+        trim: true
+    },
+    email: String,
+    notes: {
+
+    }
+})
+
+/* 
+
+User, with three fields
+
+username
+email
+a ref called notes to Note. This should be an array of objects. 
+
+*/
 
 module.exports = mongoose.model('User', userSchema)
