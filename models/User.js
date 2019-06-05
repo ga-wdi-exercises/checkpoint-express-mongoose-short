c5bb93d8d81ba7b260304dc9f4cab867cc0fe80e
Ellison Rio <rio.ellison@gmail.com>
2019-06-05 09:15:57 -0400

    added lines to both models

--- models/User.js
+++ models/User.js
@@ -1,5 +1,9 @@
 const mongoose = require('../db/connection')
 
-const userSchema = new mongoose.Schema({})
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    
+})
 
 module.exports = mongoose.model('User', userSchema)
