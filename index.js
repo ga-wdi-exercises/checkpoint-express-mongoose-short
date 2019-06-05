f1d9fa96f3cb406f90c363da3a9dec5789b7cb98
Ellison Rio <rio.ellison@gmail.com>
2019-06-05 10:09:32 -0400

    added refs to Note and User

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const parser = require("body-parser");
-
+const notesController = require("./controllers/notes")
+const usersController = require("./controllers/users")
 const app = express();
+
 app.use(parser.urlencoded({ extended: true }));
 app.use(parser.json());
+
+app.get("/", (req,res) => {res.redirect("/api/notes")})
+
+app.use("/api/notes/", notesController);
+app.use("/api/users/", usersController);
 
 app.listen(3000, () => console.log('app is running'))
 
--- models/User.js
+++ models/User.js
@@ -1,9 +1,15 @@
 const mongoose = require('../db/connection')
+const Schema = mongoose.Schema
 
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
     username: String,
     email: String,
-    
+    notes: [
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Note"
+    }
+    ]
 })
 
 module.exports = mongoose.model('User', userSchema)
