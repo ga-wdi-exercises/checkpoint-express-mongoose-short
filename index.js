61ebc77055f0f57043fe739219942e336c2584d3
Melika <melika.r@gmail.com>
2019-06-05 10:24:56 -0400

    solutions

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+
+
+const express = require('express');
+const router1 = require('./routes/router');
+const app = express();
+
 app.listen(3000, () => console.log('app is running'))
 
 // DO NOT REMOVE THIS LINE:
--- models/User.js
+++ models/User.js
@@ -1,5 +1,15 @@
+
 const mongoose = require('../db/connection')
 
-const userSchema = new mongoose.Schema({})
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    notes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Note" 
+        }
+    ]
+})
 
 module.exports = mongoose.model('User', userSchema)
