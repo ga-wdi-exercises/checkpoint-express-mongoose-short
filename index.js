3e356dbdac5ba4b196dd0c0ce15a17d692f2d692
Ellison Rio <rio.ellison@gmail.com>
2019-06-05 09:13:33 -0400

    added lines to index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const express = require("express");
+const parser = require("body-parser");
+
+const app = express();
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+
 app.listen(3000, () => console.log('app is running'))
 
 // DO NOT REMOVE THIS LINE:
