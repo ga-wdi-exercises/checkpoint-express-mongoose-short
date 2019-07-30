cd0f79226c6102759babc113a112db9c7b4ca0df
“David <davejh76@gmail.com>
2019-07-30 09:09:45 -0400

    start

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/", function(req, res) {
+  res.send("hello world");
+});
+
+
+
+
+
+
+
 app.listen(3000, () => console.log('app is running'))
+
 
 // DO NOT REMOVE THIS LINE:
 module.exports = app
