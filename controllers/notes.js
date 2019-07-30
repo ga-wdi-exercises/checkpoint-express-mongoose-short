208095d081675555b334a903a828c76b8e5778a7
Jamie Miller <miller0328@gmail.com>
2019-07-30 13:30:27 -0400

    15/18 working

--- controllers/notes.js
+++ controllers/notes.js
@@ -5,7 +5,7 @@ module.exports = {
         Note.find({}).then(notes => res.json(notes));
     },
     showNotes: function (req, res) {
-        Note.find({notes}).then(notes => res.json(notes));
+        Note.find({notes: req.params.notes}).then(notes => res.json(notes));
     },
     getNoteById: function (req, res) {
         Note.find({notes: req.params.id}).then(notes => res.json(notes));
--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ const User = require('../models/User');
 
 module.exports = {
     showUsers: function (req, res) {
-        Note.find({users}).then(notes => res.json(notes));
+        Note.find({users: req.params.users}).then(notes => res.json(notes));
     },
     getUserById: function (req, res) {
         Note.find({users: req.params.id}).then(notes => res.json(notes));
