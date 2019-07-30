e07ba198d02aa002b6a4a92150c99df0315a644e
Austin Michael Regan <austinmregan@gmail.com>
2019-07-30 09:10:38 -0400

    initialized app, is running

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-app.listen(3000, () => console.log('app is running'))
+const express = require("express");
+const parser = require("body-parser");
+const app = express();
+
+app.use(parser.json());
+
+// app.use("/api/people", require("./routes/person"));
+// app.use("/api/show", require("./routes/show"));
+
+app.listen(3000, () => console.log("app is running"));
 
 // DO NOT REMOVE THIS LINE:
-module.exports = app
+module.exports = app;
