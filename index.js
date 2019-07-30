ae72ec60da9a684ac10cf9e6c022a76c779c977e
Jerry Davidson <willdjerry@gmail.com>
2019-07-30 10:28:13 -0400

    express checkpoint

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require('express')
+const app = express()
+
+const noteController = require('./controllers/notes')
+const userController = require('./controllers/users')
+
+app.use('/',noteController)
+app.use('/', userController)
+
+
+
 app.listen(3000, () => console.log('app is running'))
 
 // DO NOT REMOVE THIS LINE:
