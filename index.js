47570435fc3affa50c769ad5a754b5f9366fb9d7
Candy Miller <candymiller@Candys-MacBook-Pro.local>
2019-09-11 10:10:47 -0400

    checkpoint

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const express = require("express");
+const app = express();
+const parser =require("body-parser");
+
+const userController=require("./controllers/users");
+const notesController=require("./controllers/notes");
+
+
+app.use(parser.urlencoded({ extended: true}));
+app.use(parser.json());
+
+app.use('/', notesController)
+app.use('/users/', userController)
+
 app.listen(3000, () => console.log('app is running'))
 
 // DO NOT REMOVE THIS LINE:
